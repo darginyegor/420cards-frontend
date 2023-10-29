4b2aa06c00a2db193769fc30742e80d2f663e44b
darginyegor <darginyegor@gmail.com>
2023-10-30 03:02:55 +0400

    add more notification styling

--- src/app/components/ui/notification/notification.component.ts
+++ src/app/components/ui/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, map, tap } from 'rxjs';
 import {
   UiNotification,
   UiNotificationsService,
@@ -12,17 +13,44 @@ import {
 export class NotificationComponent implements OnInit {
   public current: UiNotification | null = null;
   private readonly timeout = 5000;
+  public isFading = false;
+  public isHighLighted = false;
+  private _currentId = 0;
 
   constructor(private readonly notifications: UiNotificationsService) {}
 
   public ngOnInit(): void {
-    this.notifications.feed$.subscribe({
-      next: (message) => {
-        this.current = message;
-        setTimeout(() => {
-          this.current = null;
-        }, this.timeout);
-      },
-    });
+    this.notifications.feed$
+      .pipe(
+        map((event) => {
+          if (this.isFading) {
+            this.isFading = false;
+          }
+          if (this.current) {
+            this.highlight();
+          }
+          this.current = event;
+          return ++this._currentId;
+        }),
+        delay(this.timeout),
+        tap((id) => {
+          if (id === this._currentId) {
+            this.isFading = true;
+          }
+        }),
+        delay(300),
+        tap((id) => {
+          if (id === this._currentId) {
+            this.isFading = false;
+            this.current = null;
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  private highlight() {
+    this.isHighLighted = true;
+    setTimeout(() => (this.isHighLighted = false), 200);
   }
 }
